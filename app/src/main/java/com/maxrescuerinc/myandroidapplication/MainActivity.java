e43247cb9ec23fd694474fc41e5f5c65e9a9a1e4
Maxxx911 <maxrikal@gmail.com>
2018-11-07 21:19:24 +0300

    finish dev labs1

--- app/src/main/java/com/maxrescuerinc/myandroidapplication/MainActivity.java
+++ app/src/main/java/com/maxrescuerinc/myandroidapplication/MainActivity.java
@@ -1,9 +1,16 @@
 package com.maxrescuerinc.myandroidapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        daskj();
+        ShowIMEI();
+        if(!getResources().getBoolean(R.bool.dev_mode))
+        {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
-    public void daskj()
-    {
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String imei = telephonyManager.getDeviceId();
+    final private int MY_PERMISSION_ACCESS_COURSE_LOCATION = 123;
+    public void ShowIMEI() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE},  MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        } else
+            {
+            TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String imei = telephonyManager.getDeviceId();
+            TextView textView = findViewById(R.id.textView3);
+            textView.setText(imei);
+        }
     }
-
 }
